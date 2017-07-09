17b1bd39c72ed7aef52d9b3c8b96a4ec35a18bd4
Richard <richard@tinyrock.com>
2017-07-09 22:48:47 +1000

    udpated deploy server

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 bundle exec jekyll build
 
-rsync -ai --progress --delete -e 'ssh -p 8666' _site/ fedora@tinyrock.com:/var/www/richardlaugesen/
+rsync -ai --progress --delete -e 'ssh -p 8666' _site/ richard@dirac.tinyrock.com:/home/www/richardlaugesen.com/
