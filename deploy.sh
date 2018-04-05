bf3dea3133e04cfe3b8927a4a37cdf64d2756106
Richard <me@richardlaugesen.com>
2018-04-06 05:38:20 +1000

    Added gramps and updated directory name

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 bundle exec jekyll build
 
-sudo rm -rf /var/www/html/b/*
-sudo cp -rv _site/* /var/www/html/b/
+sudo rm -rf /var/www/html/c/*
+sudo cp -rv _site/* /var/www/html/c/
 
 #rsync -ai --progress --delete -e 'ssh -p 8666' _site/ richard@dirac.tinyrock.com:/home/www/richardlaugesen.com/
